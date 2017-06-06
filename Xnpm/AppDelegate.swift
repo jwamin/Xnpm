60542096528706a9464448d2a1fae6cb21738159
Joss Manger <joss.manger@langlandusa.com>
2017-06-06 19:52:20 -0400

    Initial Commit

---
+++ Xnpm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xnpm
+//
+//  Created by Joss Manger on 6/6/17.
+//  Copyright © 2017 Joss Manger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xnpm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xnpm
+//
+//  Created by Joss Manger on 6/6/17.
+//  Copyright © 2017 Joss Manger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
