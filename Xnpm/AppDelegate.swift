1633e9796776b3a29c94465fcbe2b7b4a8c2fb6c
Joss Manger <jwamin@getready.local>
2017-06-17 16:43:50 -0400

    commented touch bar code until bulk of functionality is complete

--- Xnpm/AppDelegate.swift
+++ Xnpm/AppDelegate.swift
@@ -11,10 +11,13 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    var dc:NSDocumentController!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        dc = NSDocumentController()
+        
         if #available(OSX 10.12.2, *) {
             NSApplication.shared().isAutomaticCustomizeTouchBarMenuItemEnabled = true
         } else {
@@ -33,14 +36,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         openPanel.canChooseDirectories = false;
         openPanel.begin(completionHandler: {
                 number in
-
+                self.dc.noteNewRecentDocumentURL(openPanel.url!)
                 let package = PackageAnalyser(packageUrl: openPanel.url!)
                 let main = NSStoryboard(name : "Main", bundle: nil).instantiateController(withIdentifier: "MainWindow") as! NSWindowController
                 let mainVc = NSStoryboard(name:"Main", bundle: nil).instantiateController(withIdentifier: "MainViewController") as! ViewController
                 mainVc.package = package
                 main.window?.contentViewController = mainVc
                 main.window?.makeKeyAndOrderFront(nil)
-            
+                mainVc.populateDropdown()
             
             
             
--- Xnpm/ViewController.swift
+++ Xnpm/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: NSViewController {
     @IBOutlet weak var descriptionLabel: NSTextField!
     @IBOutlet weak var license: NSTextField!
     @IBOutlet weak var repoLink: NSTextField!
-    @IBOutlet weak var scriptDropdown: NSPopUpButton!
+    @IBOutlet var scriptDropdown: NSPopUpButton!
     @IBOutlet weak var execButton: NSButton!
     @IBOutlet weak var button2: NSButton!
     @IBOutlet weak var runButton: NSButton!
@@ -23,14 +23,18 @@ class ViewController: NSViewController {
     var package:PackageAnalyser!{
         didSet{
             print("didset")
-            print(package.returndict())
+            //print(package.returndict())
+            //populateDropdown()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind(#keyPath(touchBar), to: self, withKeyPath: #keyPath(touchBar), options: nil)
+        if #available(OSX 10.12.2, *) {
+            bind(#keyPath(touchBar), to: self, withKeyPath: #keyPath(touchBar), options: nil)
+        }
         // Do any additional setup after loading the view.
+
     }
     
     override func viewDidAppear() {
@@ -38,13 +42,32 @@ class ViewController: NSViewController {
         self.view.window?.bind(#keyPath(touchBar), to: self, withKeyPath: #keyPath(touchBar), options: nil)
     }
     
+    func populateDropdown(){
+
+        scriptDropdown!.removeAllItems()
+        let main = package.returndict() as [String:Any]
+        let dict = main["scripts"] as! [String:String]
+        let scripts = Array(dict.keys)
+
+        scriptDropdown!.addItems(withTitles: scripts)
+        scriptDropdown.isEnabled = true;
+        
+//        Update Touch Bar, if available
+//        if #available(OSX 10.12.2, *) {
+//            for key in scripts {
+//                
+//                touchBar
+//            }
+//        
+//        }
+        
+        
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
-    }
-    @IBAction func runFromTouchBar(_ sender: Any) {
-        executeScript(sender: sender)
     }
 
     @IBAction func executeScript(sender:Any){
@@ -52,8 +75,16 @@ class ViewController: NSViewController {
     }
 
     deinit {
+        if #available(OSX 10.12.2, *) {
         self.view.window?.unbind(#keyPath(touchBar))
+        }
     }
     
 }
 
+//@available(OSX 10.12.2, *)
+//extension ViewController: NSTouchBarDelegate{
+//
+// 
+//    
+//}
