089609d059d4e68e332ed2da9b82b4000cc3e606
Joss Manger <jwamin@getready.fios-router.home>
2017-06-18 16:33:23 -0400

    fixed up "open recent" menu

--- Xnpm/AppDelegate.swift
+++ Xnpm/AppDelegate.swift
@@ -10,13 +10,9 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var dc:NSDocumentController!
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        
-        dc = NSDocumentController()
         
         if #available(OSX 10.12.2, *) {
             NSApplication.shared().isAutomaticCustomizeTouchBarMenuItemEnabled = true
@@ -29,26 +25,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    func handleOpen(url:URL){
+        let package = PackageAnalyser(packageUrl: url)
+        let main = NSStoryboard(name : "Main", bundle: nil).instantiateController(withIdentifier: "MainWindow") as! NSWindowController
+        let mainVc = NSStoryboard(name:"Main", bundle: nil).instantiateController(withIdentifier: "MainViewController") as! ViewController
+        mainVc.package = package
+        main.window?.contentViewController = mainVc
+        main.window?.makeKeyAndOrderFront(nil)
+    }
+    
+    func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+        let url = URL(fileURLWithPath: filename)
+        handleOpen(url: url)
+        return true
+    }
+    
     @IBAction func openAction(_ sender: Any) {
+        
         print("open called")
+        
         let openPanel = NSOpenPanel()
         openPanel.allowedFileTypes = ["json"];
         openPanel.canChooseDirectories = false;
+        
         openPanel.begin(completionHandler: {
                 number in
-                self.dc.noteNewRecentDocumentURL(openPanel.url!)
-                let package = PackageAnalyser(packageUrl: openPanel.url!)
-                let main = NSStoryboard(name : "Main", bundle: nil).instantiateController(withIdentifier: "MainWindow") as! NSWindowController
-                let mainVc = NSStoryboard(name:"Main", bundle: nil).instantiateController(withIdentifier: "MainViewController") as! ViewController
-                mainVc.package = package
-                main.window?.contentViewController = mainVc
-                main.window?.makeKeyAndOrderFront(nil)
-                //mainVc.populateDropdown()
-            
-            
+            if(openPanel.url!.pathComponents.last == "package.json"){
+                NSDocumentController.shared().noteNewRecentDocumentURL(openPanel.url!)
+                self.handleOpen(url: openPanel.url!)
+            } else {
+                print("error, not a package // should be alertdialog here")
+            }
             
         })
     }
+    
+    
 
 }
 
